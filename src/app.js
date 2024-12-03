2a3710ad600082485ce87bb4936975f91274b501
Chandankr19 <137296108+Chandankr19@users.noreply.github.com>
2024-12-04 01:49:30 +0530

    creating delete, update, and deleteById and updateById API

--- src/app.js
+++ src/app.js
@@ -2,19 +2,24 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const user = require("./models/user");
+
+// Middleware to parse JSON data
+app.use(express.json());
 
 // API to add the user into database
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Ashish",
-    lastName: "Kumar",
-    emailId: "ashishkumar@gamil.com",
-    password: "ashishkr@12",
-    age: 26,
-    gender: "male",
-  });
+  // creating a new instance of the User model
+  // const user = new User({
+  //   firstName: req.body.firstName,
+  //   lastName: req.body.lastName,
+  //   emailId: req.body.emailId,
+  //   password: req.body.password,
+  //   age: req.body.age,
+  //   gender: req.body.gender,
+  // });
 
+  // instead of above we can directly pass the Data to the database
+  const user = new User(req.body);
   try {
     const savedUser = await user.save();
     res.send("User added successfully!!");
@@ -23,14 +28,70 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-// API to get user data from database
+// API to get all the users  from database
 app.get("/users", async (req, res) => {
   try {
-    const users = await User.find();
+    const userAll = await User.find();
+
     // res.send(users);
-    res.status(200).json(users);
+    res.status(200).json(userAll);
   } catch (err) {
     res.status(500).send("Error retrieving users: " + err.message);
+  }
+});
+
+// API to get  user from their emailId
+app.get("/userByEmail", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(200).json(users);
+    }
+  } catch (err) {
+    res.status(500).send("Error while retrieving user: " + err.message);
+  }
+});
+
+//API to delete the user by userId from database
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (deletedUser) {
+      res.status(200).send(`User with ID ${userId} has been deleted`);
+    } else {
+      res.status(404).send(`User with ID ${userId} not found`);
+    }
+  } catch (err) {
+    res.status(500).send(`Error deleting user: ${err.message}`);
+  }
+});
+
+// API to update the user by userId
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const userId = req.params.id; // Extract the user ID from the URL parameters
+    const updates = req.body; // Extract the fields to update from the request body
+
+    // Using findByIdAndUpdate to find and update the user
+    const updatedUser = await User.findByIdAndUpdate(
+      userId, // find user by ID
+      updates, // Update fields provided in the request body
+      { returnDocument: "after" }  // Return the updated document
+      // { new: true } // It will also returned the updated documents
+    );
+
+    if (updatedUser) {
+      res.status(200).send(updatedUser);
+    } else {
+      res.status(404).send(`User with ID ${userId} not found`);
+    }
+  } catch (err) {
+    res.status(500).send(`Error updating user: ${err.message}`);
   }
 });
 

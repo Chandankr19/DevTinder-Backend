2435e717e4b2791f2977f07c148f166004dbb615
Chandankr19 <137296108+Chandankr19@users.noreply.github.com>
2024-12-06 17:24:57 +0530

    create logout and editProfile APIs

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -60,5 +60,10 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+// LOGOUT API
+authRouter.post("/logout", async (req, res) =>{
+    res.cookie("token", null, {expires: new Date(Date.now())});
+    res.status(200).send("Logged out successfully");
+});
 
 module.exports = authRouter;
